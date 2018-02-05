8f7a5a897b1ce5b52c17b9791b59002d8143024b
Dragu Costel <dragu.costel@yahoo.com>
2018-02-05 23:01:55 +0200

    Added security configuration

--- src/main/java/com/mdc/configuration/SecurityConf.java
+++ src/main/java/com/mdc/configuration/SecurityConf.java
@@ -1,10 +1,15 @@
 package com.mdc.configuration;
 
+import com.mdc.security.CustomAuthenticationFailureHandler;
+import com.mdc.security.CustomAuthenticationSuccessHandler;
+import com.mdc.security.CustomLogoutSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,17 +25,13 @@ import org.springframework.security.web.authentication.logout.LogoutSuccessHandl
 @EnableWebSecurity
 public class SecurityConf extends WebSecurityConfigurerAdapter {
 
-    private UserDetailsService customUserDetailsService;
-    private AuthenticationSuccessHandler customAuthenticationSuccessHandler;
-    private LogoutSuccessHandler customLogoutSuccessHandler;
-    private AuthenticationFailureHandler customAuthenticationFailureHandler;
+    private final UserDetailsService customUserDetailsService;
+    private final Environment env;
 
     @Autowired
-    public SecurityConf(UserDetailsService customUserDetailsService, AuthenticationSuccessHandler customAuthenticationSuccessHandler, LogoutSuccessHandler customLogoutSuccessHandler, AuthenticationFailureHandler customAuthenticationFailureHandler) {
+    public SecurityConf(UserDetailsService customUserDetailsService, Environment env) {
         this.customUserDetailsService = customUserDetailsService;
-        this.customAuthenticationSuccessHandler = customAuthenticationSuccessHandler;
-        this.customLogoutSuccessHandler = customLogoutSuccessHandler;
-        this.customAuthenticationFailureHandler = customAuthenticationFailureHandler;
+        this.env = env;
     }
 
     @Override
@@ -45,8 +46,58 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
         web.ignoring().antMatchers("/resources/**");
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(env.getProperty("location.resources")).permitAll()
+                .antMatchers(env.getProperty("location.test")).permitAll()
+                .antMatchers(env.getProperty("location.login")).access("hasRole('ANONYMOUS')")
+                .antMatchers(env.getProperty("location.logout")).access("hasRole('USER')")
+                .antMatchers(env.getProperty("location.global")).access("hasRole('USER')")
+
+                .and().exceptionHandling().accessDeniedPage(env.getProperty("location.access_denied"))
+
+                .and().formLogin()
+                .loginPage(env.getProperty("login.url"))
+                .loginProcessingUrl(env.getProperty("login.processing.url"))
+                .failureUrl(env.getProperty("login.failure.url"))
+                .usernameParameter(env.getProperty("login.username.parameter"))
+                .passwordParameter(env.getProperty("login.password.parameter"))
+                .successHandler(customAuthenticationSuccessHandler())
+                .failureHandler(customAuthenticationFailureHandler())
+                .defaultSuccessUrl(env.getProperty("default.success.url"))
+
+                .and().logout()
+                .logoutUrl(env.getProperty("logout.url"))
+                .logoutSuccessUrl(env.getProperty("logout.success.url"))
+                .logoutSuccessHandler(customLogoutSuccessHandler())
+                .deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true)
+
+                .and().httpBasic()
+                .and().csrf().disable();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(11);
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler customAuthenticationSuccessHandler() {
+        AuthenticationSuccessHandler customAuthenticationSuccessHandler = new CustomAuthenticationSuccessHandler();
+        return customAuthenticationSuccessHandler;
+    }
+
+    @Bean
+    public AuthenticationFailureHandler customAuthenticationFailureHandler() {
+        AuthenticationFailureHandler customAuthenticationFailureHandler = new CustomAuthenticationFailureHandler();
+        return customAuthenticationFailureHandler;
+    }
+
+    @Bean
+    public LogoutSuccessHandler customLogoutSuccessHandler() {
+        LogoutSuccessHandler customLogoutSuccessHandler = new CustomLogoutSuccessHandler();
+        return customLogoutSuccessHandler;
     }
 }
--- src/main/java/com/mdc/initializer/WebSecurityInitializer.java
+++ src/main/java/com/mdc/initializer/WebSecurityInitializer.java
@@ -3,6 +3,9 @@ package com.mdc.initializer;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
+/***
+ * Initialize web security
+ */
 @Order(1)
 public class WebSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
     public WebSecurityInitializer() {
